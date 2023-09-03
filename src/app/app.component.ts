b691821d13db6323a147c49424fee6534e126ee3
Mohammed Khan <abukhan363@gmail.com>
2023-09-03 13:17:04 -0400

    Toggle navigation using arrows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title: string = 'portfolio';
   themes: string[] = [];
   currThemeIndex: number = 0;
-  currStep: number = 0;
+
+   // bottom navigation step number
+  currStepNumber: number = 0;
+  maxStepNumber: number = 5;
   
   ngOnInit() {
     this.themes = ['retro', 'cyberpunk',  'aqua', 'forest'];
@@ -19,5 +22,26 @@ export class AppComponent {
   toggleThemes() {
     this.currThemeIndex = (this.currThemeIndex + 1) % this.themes.length;
     document.body.setAttribute('data-theme', this.themes[this.currThemeIndex]);
+  }
+
+  get getCurrentThemeText() {
+    return `Current Theme: ${this.themes[this.currThemeIndex]}`;
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.key === "ArrowLeft") {
+      console.log(`left arrow pressed - curr step ${this.currStepNumber}`);
+      if (this.currStepNumber > 0) {
+        this.currStepNumber -= 1;
+      }
+    } else if (event.key == "ArrowRight") {
+      console.log(`right arrow pressed - curr step ${this.currStepNumber}`);
+      if (this.currStepNumber < this.maxStepNumber) {
+        this.currStepNumber += 1;
+      }
+    } else if (event.key == "f" || event.key == "F") {
+      alert("Respects paid!");
+    }
   }
 }
