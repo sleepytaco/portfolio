da7bd76d657f353b45992ebfbd6113f6c45a03a0
Mohammed Khan <abukhan363@gmail.com>
2023-09-05 11:18:06 -0400

    Fill in experience page

--- src/app/portfolio/experience-page/experience-page.component.ts
+++ src/app/portfolio/experience-page/experience-page.component.ts
@@ -6,6 +6,7 @@ type Experience = {
   company: string,
   duration: string,
   desc: string[],
+  skills: string[]
 };
 
 @Component({
@@ -20,7 +21,20 @@ export class ExperiencePageComponent {
       "title": "Computer Science Teaching Assistant",
       "company": "Brown University",
       "duration": "Aug 2023 - Present",
-      "desc": ["Responsible for grading homework and organizing weekly office hours to help students with assignments involving search, hidden markov models, machine learning, and reinforcement learning."]
+      "desc": [
+        "Teaching assistant for CS1410: Artificial Intelligence (Fall 2023)",
+        "Organized weekly office hours to help students with assignments involving search, hidden markov models, machine learning, and reinforcement learning.",
+        "Responsible for mentoring students on their final project which invovles coding an AI bot to beat a game."
+      ], 
+      "skills": [
+        "Python",
+        "NumPy",
+        "Machine Learning",
+        "Reinforcement Learning",
+        "Teaching",
+        "Mentoring",
+        "Time Management"
+      ],
     },
     {
       "ongoing": false,
@@ -30,7 +44,19 @@ export class ExperiencePageComponent {
       "desc": [
         "Helped develop and deploy their Django backend on Azure and worked with technologies like SQL and Redis as per the requirements of the Codeucate student portal.",
         "Collaborated with front-end developers to integrate user-facing elements with server-side logic, integrated features like instructor onboarding, referral system, certificate generator, and a payments system using the PayPal API."
-      ]
+      ],
+      "skills": [
+        "Python",
+        "Django",
+        "JavaScript",
+        "Integrating APIs",
+        "Microsoft Azure",
+        "SQL",
+        "Redis",
+        "Collaboration",
+        "Time Management",
+        "Code Refactoring/Documentation",
+      ],
     },
     {
       "ongoing": false,
@@ -41,7 +67,15 @@ export class ExperiencePageComponent {
         "Responsible for teaching CodePath's Android Development course at Penn State via weekly sessions where the students developed 4 Android apps throughout a span of 10 weeks.",
         "Mentored and supported a class of 14 students by providing technical support during labs and assignments to ensure they are on track with the pace of the course.",
         "Brought up student enrollment by marketing the course on-campus by reaching out to several Computer Science and Engineering professors and clubs.",
-      ]
+      ],
+      "skills": [
+        "Java",
+        "Android Development",
+        "Communication",
+        "Teaching",
+        "Facilitation",
+        "Classroom Management",
+      ],
     },
     {
       "ongoing": false,
@@ -49,9 +83,16 @@ export class ExperiencePageComponent {
       "company": "Medtronic, Plc",
       "duration": "Aug 2021 - Dec 2021",
       "desc": [
+        "Penn State Computer Engineering capstone project sponsored by Medtronic, plc.",
         "Collaborated with a team of four to build a custom motion capture Android app utilizing the XsensDOT SDK to establish real-time connections with up to three wireless sensors and display live arm angle calculations on the screen.",
         "Worked closely with Medtronic, plc sponsors to optimize the app's performance, coordinating regular meetings to analyze and troubleshoot the raw data latency issues, resulting in a 50ms reduction in background data collection latency and improving the overall user experience."
-      ]
+      ],
+      "skills": [
+        "Java",
+        "Android Development",
+        "Communication",
+        "Team Leadership",
+      ],
     },
     {
       "ongoing": false,
@@ -60,7 +101,10 @@ export class ExperiencePageComponent {
       "duration": "Aug 2021 - Dec 2021",
       "desc":[
         "Graded assignments covering data structures and algorithmic design for over 300 students."
-      ]
+      ],
+      "skills": [
+        "Algorithms & Data Structures"
+      ],
     },
   ];
 }
