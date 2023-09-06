f236a992874c869fd837fdd97bbf7382f9f9242b
Mohammed Khan <abukhan363@gmail.com>
2023-09-06 12:55:50 -0400

    Firebase env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase:  {
+        apiKey: "AIzaSyDUg9wt8LZfl11-g7Kg-QxkT83wugJJCEM",
+        authDomain: "im-mohammedk.firebaseapp.com",
+        projectId: "im-mohammedk",
+        storageBucket: "im-mohammedk.appspot.com",
+        messagingSenderId: "555054352739",
+        appId: "1:555054352739:web:e875c087651f6128a9ab56",
+        measurementId: "G-W0CP68NP7G"
+  }
+};
