4c936ba64beee09ad21de33afc08e52bc4a12cd8
Mohammed Khan <abukhan363@gmail.com>
2023-09-07 09:49:06 -0400

    Move confetti script inside Bottom Nav Component

--- src/app/bottom-nav/bottom-nav.component.ts
+++ src/app/bottom-nav/bottom-nav.component.ts
@@ -4,6 +4,8 @@ import { ThemesService } from '../services/themes.service';
 import { Title } from "@angular/platform-browser";
 import { Location } from '@angular/common';
 
+declare function confetti(obj: any): void;
+
 @Component({
   selector: 'app-bottom-nav',
   templateUrl: './bottom-nav.component.html',
@@ -48,6 +50,38 @@ export class BottomNavComponent implements OnInit {
        }
        this._router.navigateByUrl(this.links[this.currStepNumber]);
        this.titleService.setTitle(this.pageTitles[this.currStepNumber]);
+     } else if (this.currStepNumber != 4 && (event.key == "F" || event.key == "f")) { // don't blast confetti on contact page
+        const duration = 7 * 1000,
+          animationEnd = Date.now() + duration,
+          defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        const randomInRange = (min: number, max: number) => {
+          return Math.random() * (max - min) + min;
+        }
+
+        const interval: any = setInterval(function() {
+          const timeLeft = animationEnd - Date.now();
+
+          if (timeLeft <= 0) {
+            return clearInterval(interval);
+          }
+
+          const particleCount = 50 * (timeLeft / duration);
+
+          // since particles fall down, start a bit higher than random
+          confetti(
+            Object.assign({}, defaults, {
+              particleCount,
+              origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+            })
+          );
+          confetti(
+            Object.assign({}, defaults, {
+              particleCount,
+              origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+            })
+          );
+        }, 250);
      }
    }
 }
